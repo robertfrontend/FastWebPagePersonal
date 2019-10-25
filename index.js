6e508a0b9764c1868d50bc59a6eb40fc42dd45be
Robert Romero <enanow41romero@gmail.com>
2019-10-25 01:17:46 -0400

    Version un poco responsive

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("btn-question").addEventListener("click",function(){
+  
+document.getElementById("div-respuesta").classList.toggle("activar-div")
+});
+
+
+
+
+document.getElementById("btn-menu").addEventListener("click",function(){
+  
+document.getElementById("div-link").classList.toggle("activar-menu")
+});
+
+    
